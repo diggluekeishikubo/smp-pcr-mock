4ac9804d6602184fe3743e49b370c42795dcd61b
admin <keishi.kubo@digglue.com>
2021-01-25 17:55:30 +0900

    update

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h1 class="main-title">コロナの心配があるかたはこちらから</h1>
     <div style="text-align: center; display: flex; flex-direction: column; align-items: center">
-      <a class="button" mat-flat-button color="primary" routerLink="/order-recycle">問診を受けて<br>検査の必要性を確認する</a>
+      <a class="button" mat-flat-button color="primary" routerLink="/order-recycle">検査の必要性を確認したい</a>
       <a class="button" mat-flat-button color="primary" routerLink="/order-recycle">PCR検査を受けたい</a>
     </div>
   `,
