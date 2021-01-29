6c81a94bae65cc56ad40459ffea7d82ddab0ab02
admin <keishi.kubo@digglue.com>
2021-01-29 21:09:33 +0900

    update mock

--- src/app/main/bulletin-board/shops.component.ts
+++ src/app/main/bulletin-board/shops.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Item, ItemsService } from '../../services/items.service';
 
 @Component({
   selector: 'app-shops',
@@ -16,23 +15,74 @@ import { Item, ItemsService } from '../../services/items.service';
     </header>
     <h1 class="main-title">経験をシェアしましょう</h1>
     <p style="text-align: center">困ったことや役に立ったことなどをシェアしましょう。あなたの体験が誰かの役に立ちます。</p>
+    <mat-card class="example-card" *ngFor="let c of comments">
+      <mat-card-header>
+        <div mat-card-avatar class="example-header-image"></div>
+        <mat-card-title>{{c.title}}</mat-card-title>
+        <mat-card-subtitle>{{c.name}}</mat-card-subtitle>
+      </mat-card-header>
+      <mat-card-content>
+        <p>{{c.comment}}</p>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-button>いいね</button>
+        <button mat-button>コメントをする</button>
+      </mat-card-actions>
+    </mat-card>
   `,
   styleUrls: [ './shops.component.scss' ]
 })
 export class ShopsComponent implements OnInit {
-  constructor(
-    private itemsService: ItemsService
-  ) {
-  }
 
-  get items(): Item[] {
-    return this.itemsService.list();
-  }
+  comments = [
+    {
+      title: '家族がいるかたでも気をつけた方がよいです',
+      name: 'Aさん40歳男性',
+      comment: '「症状は軽かったのですが、家族全員が濃厚接触者となり息子は中学入試を受けられなくなりました。感染率が低くても家族に大きな影響を与えるので、特に大事な時期は外出を控えるべきと思いました。みなさんも気を付けてください」\n'
+    },
+    {
+      title: '診断を受けてみると',
+      name: 'Cさん20歳男性。。',
+      comment: '「熱が出たので検査を受けました。はじめは検査をどうやって受ければ良いかわからないと思うので、とりあえず”検査を受けるべきか問診を受けてみる”ボタンを押せば診断してもらえます。」'
+    },
+    {
+      title: '膝が5日間痛むのですが、PCR検査を受けた方がよいでしょうか？',
+      name: 'Cさん20歳男性。',
+      comment: '「膝の関節が痛むのですが、同じ症状の人いますか？検査するか迷ってます。」'
+    },
+    {
+      title: '家族がいるかたでも気をつけた方がよいです',
+      name: 'Aさん40歳男性',
+      comment: '「症状は軽かったのですが、家族全員が濃厚接触者となり息子は中学入試を受けられなくなりました。感染率が低くても家族に大きな影響を与えるので、特に大事な時期は外出を控えるべきと思いました。みなさんも気を付けてください」\n'
+    },
+    {
+      title: '診断を受けてみると',
+      name: 'Cさん20歳男性。。',
+      comment: '「熱が出たので検査を受けました。はじめは検査をどうやって受ければ良いかわからないと思うので、とりあえず”検査を受けるべきか問診を受けてみる”ボタンを押せば診断してもらえます。」'
+    },
+    {
+      title: '膝が5日間痛むのですが、PCR検査を受けた方がよいでしょうか？',
+      name: 'Cさん20歳男性。',
+      comment: '「膝の関節が痛むのですが、同じ症状の人いますか？検査するか迷ってます。」'
+    },
+    {
+      title: '家族がいるかたでも気をつけた方がよいです',
+      name: 'Aさん40歳男性',
+      comment: '「症状は軽かったのですが、家族全員が濃厚接触者となり息子は中学入試を受けられなくなりました。感染率が低くても家族に大きな影響を与えるので、特に大事な時期は外出を控えるべきと思いました。みなさんも気を付けてください」\n'
+    },
+    {
+      title: '診断を受けてみると',
+      name: 'Cさん20歳男性。。',
+      comment: '「熱が出たので検査を受けました。はじめは検査をどうやって受ければ良いかわからないと思うので、とりあえず”検査を受けるべきか問診を受けてみる”ボタンを押せば診断してもらえます。」'
+    },
+    {
+      title: '膝が5日間痛むのですが、PCR検査を受けた方がよいでしょうか？',
+      name: 'Cさん20歳男性。',
+      comment: '「膝の関節が痛むのですが、同じ症状の人いますか？検査するか迷ってます。」'
+    },
+  ];
 
-  getItemDetail(item: { detail: string }): string {
-    const length = item.detail.length;
-    const suffix = 15 < length ? '...' : '';
-    return item.detail.slice(0, 15) + suffix;
+  constructor() {
   }
 
   ngOnInit(): void {
